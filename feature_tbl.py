67faa2e0665792aae2bd65864bb7aea2895a2a4e
suyash308 <suyash308@gmail.com>
2019-07-12 23:22:54 +0530

    Add files via upload

---
+++ feature_tbl.py
@@ -0,0 +1,12 @@
+sfdcipoc@2019
+
+jdbc:mysql://localhost:3306/?user=root
+
+avharwani@deloitte.com.sfdcipoc.dipune
+sfdcipoc@2019
+bG756bL3Vp38oniys1OOD0td
+https://test.salesforce.com/services/Soap/u/43.0
+
+
+
+SpWbslpdbKLJl3kEtKe64gMM0
