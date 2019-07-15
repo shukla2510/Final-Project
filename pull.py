48a67852d16eaf5965274edef62dc63fdc9dcfdb
suyash308 <suyash308@gmail.com>
2019-07-15 12:42:45 +0530

    Update pull.py
    without INI

--- pull.py
+++ pull.py
@@ -8,41 +8,8 @@ def summarize(message):
     # [START parse_message]
     data = message.data.decode('utf-8')
     attributes = message.attributes
-
-    event_type = attributes['eventType']
     bucket_id = attributes['bucketId']
     object_id = attributes['objectId']
-    generation = attributes['objectGeneration']
-    description = (
-        '\tEvent type: {event_type}\n'
-        '\tBucket ID: {bucket_id}\n'
-        '\tObject ID: {object_id}\n'
-        '\tGeneration: {generation}\n').format(
-            event_type=event_type,
-            bucket_id=bucket_id,
-            object_id=object_id,
-            generation=generation)
-
-    if 'overwroteGeneration' in attributes:
-        description += '\tOverwrote generation: %s\n' % (
-            attributes['overwroteGeneration'])
-    if 'overwrittenByGeneration' in attributes:
-        description += '\tOverwritten by generation: %s\n' % (
-            attributes['overwrittenByGeneration'])
-
-    payload_format = attributes['payloadFormat']
-    if payload_format == 'JSON_API_V1':
-        object_metadata = json.loads(data)
-        size = object_metadata['size']
-        content_type = object_metadata['contentType']
-        metageneration = object_metadata['metageneration']
-        description += (
-            '\tContent type: {content_type}\n'
-            '\tSize: {object_size}\n'
-            '\tMetageneration: {metageneration}\n').format(
-                content_type=content_type,
-                object_size=size,
-                metageneration=metageneration)
     return bucket_id, object_id
     # [END parse_message]
 
@@ -58,10 +25,9 @@ def poll_notifications(project, subscription_name):
         print('Received message:\n{}'.format(bucket_id))
         print('Received message:\n{}'.format(object_id))
         f = 'gs://'+str(bucket_id)+'/'+str(object_id)
-        cmd = "spark-submit a.py"
+        cmd = "spark-submit a.py "+str(f)
         print(cmd)
-        returned_value = os.system(cmd)
-        print(f)
+        # returned_value = os.system(cmd)
         message.ack()
 
     subscriber.subscribe(subscription_path, callback=callback)
