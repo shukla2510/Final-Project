2d459676590820cd7413f75706071af3eed2c5b8
amoldarkunde31 <51758674+amoldarkunde31@users.noreply.github.com>
2019-07-26 15:25:16 +0530

    Update program.py

--- program.py
+++ program.py
@@ -8,7 +8,7 @@ from pyspark.sql.functions import lit
 
 class program_data:
     def save_data(self, file_name, raw_zone_bucket, refined_zone_bucket):
-        pddf = spark.read.text(file_name)
+        pddf = spark.read.option("delimiter",";").csv(file_name, header=True)
         json_name = file_name.rsplit('.', 1)[0].rsplit('/', 1)[1]
         raw_zone_path = 'gs://{}/{}/'.format(raw_zone_bucket, json_name)
         pddf.write.mode('append').parquet(raw_zone_path)
