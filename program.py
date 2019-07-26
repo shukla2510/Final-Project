19ee881a32c2a35dad48b7ac198dc2c628a069f6
amoldarkunde31 <51758674+amoldarkunde31@users.noreply.github.com>
2019-07-26 15:11:51 +0530

    Update program.py

--- program.py
+++ program.py
@@ -12,38 +12,32 @@ class program_data:
         json_name = file_name.rsplit('.', 1)[0].rsplit('/', 1)[1]
         raw_zone_path = 'gs://{}/{}/'.format(raw_zone_bucket, json_name)
         pddf.write.mode('append').parquet(raw_zone_path)
-        mompar = spark.read.parquet(raw_zone_path)
+        progpar = spark.read.parquet(raw_zone_path)
 
-        program_tab=progpar.withColumn("col1", split(col("value"), "\\;").getItem(0)) \
-        .withColumn("Channel", split(col("value"), "\\;").getItem(1)) \
-        .withColumn("Episode_Date", split(col("value"), "\\;").getItem(2)) \
-        .withColumn("Description", split(col("value"), "\\;").getItem(3)) \
-        .withColumn("Programme_Theme", split(col("value"), "\\;").getItem(4)) \
-        .withColumn("Programme_Genre", split(col("value"), "\\;").getItem(5)) \
-        .withColumn("Level", split(col("value"), "\\;").getItem(6)) \
-        .withColumn("Start_Time", split(col("value"), "\\;").getItem(7)) \
-        .withColumn("End_Time", split(col("value"), "\\;").getItem(8)) \
-        .withColumn("Length", split(col("value"), "\\;").getItem(9)) \
-        .withColumn("HSM_Universe", split(col("value"), "\\;").getItem(10)) \
-        .withColumn("HSM_15_plus", split(col("value"), "\\;").getItem(11)) \
-        .withColumn("HSM_Female_15_plus_AB", split(col("value"), "\\;").getItem(12)) \
-        .withColumn("HSM_Urban_Universe", split(col("value"), "\\;").getItem(13)) \
-        .withColumn("HSM_Urban_15_plus", split(col("value"), "\\;").getItem(14)) \
-        .withColumn("HSM_Urban_Female_15_plus_AB", split(col("value"), "\\;").getItem(15)) \
-        .withColumn("HSM_Rural_Universe", split(col("value"), "\\;").getItem(16)) \
-        .withColumn("HSM_Rural_15_plus", split(col("value"), "\\;").getItem(17)) \
-        .withColumn("HSM_Rural_Female_15_plus_AB", split(col("value"), "\\;").getItem(18)) \
-        .withColumn("Mega_Cities_Universe", split(col("value"), "\\;").getItem(19)) \
-        .withColumn("Mega_Cities_15_plus", split(col("value"), "\\;").getItem(20)) \
-        .withColumn("Mega_Cities_Female_15_plus", split(col("value"), "\\;").getItem(21))
-    
-        def remove_end_quote(col):
-            return col.rsplit('"')[0]
-        remove_end_quote_udf = F.UserDefinedFunction(remove_end_quote, T.StringType())
+        program_tab=progpar.withColumnRenamed('Channel','Channel')\
+        .withColumnRenamed('Date','Episode_Date')\
+        .withColumnRenamed('Description','Description')\
+        .withColumnRenamed('Programme Theme','Programme_Theme')\
+        .withColumnRenamed('Programme Genre','Programme_Genre')\
+        .withColumnRenamed('Level','Level')\
+        .withColumnRenamed('Start Time {Av(Tm)}','Start_Time')\
+        .withColumnRenamed('End Time {Av(Tm)}','End_Time')\
+        .withColumnRenamed('Length [hhmmss] {Av}','Length')\
+        .withColumnRenamed('HSM','HSM_Universe')\
+        .withColumnRenamed(' 11','HSM_15_plus')\
+        .withColumnRenamed(' 12','HSM_Female_15_plus_AB')\
+        .withColumnRenamed('HSM Urban','HSM_Urban_Universe')\
+        .withColumnRenamed(' 14','HSM_Urban_15_plus')\
+        .withColumnRenamed(' 15','HSM_Urban_Female_15_plus_AB')\
+        .withColumnRenamed('HSM Rural','HSM_Rural_Universe')\
+        .withColumnRenamed(' 17','HSM_Rural_15_plus')\
+        .withColumnRenamed(' 18','HSM_Rural_Female_15_plus_AB')\
+        .withColumnRenamed('Mega Cities','Mega_Cities_Universe')\
+        .withColumnRenamed(' 20','Mega_Cities_15_plus')\
+        .withColumnRenamed(' 21','Mega_Cities_Female_15_plus')\
+        .drop(' 0')
 
-        # mom_tab_stag = program_tab.drop('value')
-        program_tab_final=program_tab.where((program_tab.col1 != '" ') & (program_tab.col1 !='"[TOTAL]'))
-        program_tab_final = program_tab_final.withColumn('Mega_Cities_Female_15_plus_new', remove_end_quote_udf(program_tab_final.Mega_Cities_Female_15_plus)).drop('Mega_Cities_Female_15_plus').drop('col1')
+        program_tab_final=program_tab.where((program_tab.Channel != ' ') & (program_tab.Channel !='"[TOTAL]'))  
         program_tab_final.write.mode('append').parquet('gs://{}/program_data/'.format(refined_zone_bucket))
 
 if __name__ == '__main__':
